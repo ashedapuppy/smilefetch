55a6e0f24b926af2459131fc443abe239a37fdf3
sudosmile <mael.leclerc@epitech.eu>
2021-01-04 00:33:00 +0100

    {added}{others} colour

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include "include/main.h"
 
+#define reset "\x1b[0m"
+#define bold "\x1b[1m"
+#define black "\x1b[30m"
+#define red "\x1b[31m"
+#define green "\x1b[32m"
+#define yellow "\x1b[33m"
+#define blue "\x1b[34m"
+#define magenta "\x1b[35m"
+#define cyan "\x1b[36m"
+#define white "\x1b[37m"
+
 int main(int arc, char **arv)
 {
     char *os_name = malloc(sizeof(char) * 100);
@@ -14,11 +25,11 @@ int main(int arc, char **arv)
     get_user(&user);
     get_cpuinfo(&cpu_name);
 
-    fprintf(stdout,"user: \t\t%s\n",user);
-    fprintf(stdout,"distro: \t%s\n", os_name);
-    fprintf(stdout,"kernel: \t%s\n", kernel_name);
-    fprintf(stdout,"uptime: \t%s seconds\n", uptime);
-    fprintf(stdout,"cpu: \t\t%s\n", cpu_name);
+    fprintf(stdout,red "user" reset ":" "\t\t%s\n",user);
+    fprintf(stdout,red "distro" reset ":" "\t\t%s\n", os_name);
+    fprintf(stdout,red "kernel" reset ":" "\t\t%s\n", kernel_name);
+    fprintf(stdout,red "uptime" reset ":" "\t\t%s seconds\n", uptime);
+    fprintf(stdout,red "cpu" reset ":" "\t\t%s\n", cpu_name);
     exit(0);
 }
 
