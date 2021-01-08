4b95a70e0bca03483e65332a9fce84bb1c9d6f97
sudosmile <mael.leclerc@epitech.eu>
2021-01-08 04:58:43 +0100

    {modified} tried to shorten a few functions

--- getdata.c
+++ getdata.c
@@ -32,42 +32,26 @@ void get_kernel(char **kernel_name)
     fclose(kernel_file);
 }
 
-/*
- *void get_model(char **model_name, char **model_version)
- *{
- *    FILE *model_name_file;
- *    FILE *model_version_file;
- *    if ((model_name_file = fopen("/sys/devices/virtual/dmi/id/product_name", "r")) == NULL) {
- *        fprintf(stderr, "error opening /sys/devices/virtual/dmi/id/product_name");
- *        exit(1);
- *    }
- *    if ((model_name_file = fopen("/sys/devices/virtual/dmi/id/product_version", "r")) == NULL) {
- *        fprintf(stderr, "error opening /sys/devices/virtual/dmi/id/product_version");
- *        exit(1);
- *    }
- *}
- */
-
 void get_uptime(char **uptime)
 {
     FILE *uptime_file;
-    if ((uptime_file = fopen("/proc/uptime", "r")) == NULL)
-    {
+    if ((uptime_file = fopen("/proc/uptime", "r")) == NULL) {
         fprintf(stderr, "error opening /proc/uptime\n");
         exit(1);
     }
     fscanf(uptime_file, "%[^ ]", *uptime);
-    long uptime_long = strtol((*uptime), NULL, 10);
-    long uptime_days = uptime_long / 86400; // /60 / 60 / 24 to get num of hours
-    long uptime_hours = uptime_long / 3600 % 24; // /60 /60 %24 to get hours - days
-    long uptime_minutes = uptime_long / 60 % 60; // minutes - hours
-    long uptime_seconds = uptime_long % 60;
-    if (uptime_days != 0) {
-        sprintf(*uptime, "%ld days, %ld hours, %ld minutes", uptime_days, uptime_hours, uptime_minutes);
-    } else if (uptime_hours != 0) {
-        sprintf(*uptime, "%ld hours, %ld minutes", uptime_hours, uptime_minutes);
-    } else if (uptime_minutes != 0) {
-        sprintf(*uptime, "%ld minutes, %ld seconds", uptime_minutes, uptime_seconds);
+    long up_long = strtol((*uptime), NULL, 10);
+    long up_days = up_long / 86400;
+    long up_hours = up_long / 3600 % 24;
+    long up_minutes = up_long / 60 % 60;
+    long up_seconds = up_long % 60;
+    if (up_days != 0) {
+        sprintf(*uptime, "%ld days, %ld hours, %ld minutes",
+                up_days, up_hours, up_minutes);
+    } else if (up_hours != 0) {
+        sprintf(*uptime, "%ld hours, %ld minutes", up_hours, up_minutes);
+    } else if (up_minutes != 0) {
+        sprintf(*uptime, "%ld minutes, %ld seconds", up_minutes, up_seconds);
     } else {
         fprintf(stderr, "Uptime = 0? sounds like an error\n");
         exit(1);
@@ -79,14 +63,12 @@ void get_shell(char **shell, char *user)
 {
     FILE *shell_file;
     char *line = malloc(1024);
-    if ((shell_file = fopen("/etc/passwd", "r")) == NULL)
-    {
+    if ((shell_file = fopen("/etc/passwd", "r")) == NULL) {
         fprintf(stderr, "error opening /etc/passwd\n");
         exit(1);
     }
-    while (fgets(line, 1024, shell_file) != NULL)
-    {
-        if (strstr(line, user)) {
+    while (fgets(line, 1024, shell_file) != NULL) {
+        if (strstr(line, user)) { //this file is consistent so no need to check
             (*shell) = line;
             if (strstr((*shell), "bash"))
                 (*shell) = "bash";
@@ -131,14 +113,12 @@ void get_cpuinfo(char **cpu_name)
 {
     FILE *cpu_name_file;
     char *line = malloc(1024);
-    if ((cpu_name_file = fopen("/proc/cpuinfo", "r")) == NULL)
-    {
+    if ((cpu_name_file = fopen("/proc/cpuinfo", "r")) == NULL) {
         fprintf(stderr, "error opening /proc/cpuinfo\n");
         exit(1);
     }
-    while (fgets(line, 1024, cpu_name_file) != NULL)
-    {
-        if(strstr(line, "model name")){
+    while (fgets(line, 1024, cpu_name_file) != NULL) {
+        if(strstr(line, "model name")) {
             (*cpu_name) = line;
             for(; (*cpu_name)[0] != ':';)
                 (*cpu_name)++;
